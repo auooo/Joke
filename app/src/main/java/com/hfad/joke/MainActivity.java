b2a8d6748497645e202e8f0c197bcd6bcbf8e708
auooo <mh245500@gmail.com>
2016-01-03 22:03:51 +0800

    Initial commit

---
+++ app/src/main/java/com/hfad/joke/MainActivity.java
@@ -0,0 +1,27 @@
+package com.hfad.joke;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, DelayedMessageService.class);
+        intent.putExtra(DelayedMessageService.EXTRA_MESSAGE,
+                getResources().getString(R.string.button_response));
+        startService(intent);
+    }
+}
