969f0f250a1e9a2da72c99c384a9bff13c5b01a8
auooo <mh245500@gmail.com>
2016-01-03 22:19:28 +0800

    change from Log to Toast

--- app/src/main/java/com/hfad/joke/DelayedMessageService.java
+++ app/src/main/java/com/hfad/joke/DelayedMessageService.java
@@ -2,15 +2,27 @@ package com.hfad.joke;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.Toast;
 
 public class DelayedMessageService extends IntentService {
 
     // Use a constant to pass a message from the activity to the service
     public static final String EXTRA_MESSAGE = "message";
+    private Handler handler;
 
     public DelayedMessageService() {
         super("DelayedMessageService");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Create the handler on the main thread
+        handler = new Handler();
+        // It's a must to call the super implementation for intent service to properly handle the
+        // life of its background thread.
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
@@ -27,6 +39,11 @@ public class DelayedMessageService extends IntentService {
     }
 
     private void showText(final String text) {
-        Log.v("DelayedMessageService", "The message is: " + text);
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
